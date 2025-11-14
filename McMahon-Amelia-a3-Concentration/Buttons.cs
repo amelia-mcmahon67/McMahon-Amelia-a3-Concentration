eb3c4b827b145f2fbac1111cb8cb13597ea67c44
amelia-mcmahon67 <amelia.mcmahon@mohawkcollege.ca>
2025-11-13 23:16:52 -0500

    wrote out rules screen

--- McMahon-Amelia-a3-Concentration/Buttons.cs
+++ McMahon-Amelia-a3-Concentration/Buttons.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MohawkGame2D;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,41 @@ namespace McMahon_Amelia_a3_Concentration
 {
     public class Buttons
     {
+        Vector2 size;
+        Vector2 position;
+        private string text;
+
+        public Buttons(Vector2 position, Vector2 size, string text)
+        {
+            this.position = position;
+            this.size = size;
+            this.text = text;
+        }
+        public void Update()
+        {
+            drawButton();
+        }
+        public void drawButton()
+        {
+            Draw.FillColor = Color.Gray;
+            Draw.Rectangle(position, size);
+            Text.Size = 30;
+            Text.Draw(text, position);
+        }
+        //checks if button is clicked
+        public bool ClickCheck()
+        {
+            Vector2 mousePosition = Input.GetMousePosition();
+            bool buttonPressed = false;
+            if (mousePosition.X > position.X && mousePosition.X < position.X + size.X && mousePosition.Y > position.Y && mousePosition.Y < position.Y + size.Y && Input.IsMouseButtonPressed(0))
+            {
+                buttonPressed = true;
+                return buttonPressed;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
--- McMahon-Amelia-a3-Concentration/Game.cs
+++ McMahon-Amelia-a3-Concentration/Game.cs
@@ -11,14 +11,25 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
+        bool titleScreen;
+        bool gameGoing;
+        bool rulesScreen;
 
-
+        //min 30 tall
+        Buttons[] titleScreenButtons =
+        {
+            new Buttons(new Vector2(350, 385), new Vector2(100, 30), new string("PLAY")),
+            new Buttons(new Vector2(350, 425), new Vector2(100, 30), new string("RULES"))
+        };
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-
+            Window.SetSize(800, 600);
+            titleScreen = true;
+            gameGoing = false;
+            rulesScreen = false;
         }
 
         /// <summary>
@@ -26,7 +37,38 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.White);
+            if (titleScreen == true)
+            {
+                //drawing Title
+                Text.Size = 70;
+                Text.Draw(new string("Colours"), new Vector2(255, 100));
+                Text.Draw(new string("Concentration"), new Vector2(150, 170));
+                //button drawing
+                for (int i = 0; i < titleScreenButtons.Length; i++)
+                {
+                    titleScreenButtons[i].Update();
+                }
+                //buttons being checked if theyre clicked
+                if (titleScreenButtons[0].ClickCheck())
+                {
+                    gameGoing = true;
+                    titleScreen = false;
+                }
+                if (titleScreenButtons[1].ClickCheck())
+                {
+                    rulesScreen = true;
+                    titleScreen = false;
+                }
+            }
+            else if (gameGoing == true)
+            {
 
+            }
+            else if (rulesScreen == true)
+            {
+
+            }
         }
     }
 
